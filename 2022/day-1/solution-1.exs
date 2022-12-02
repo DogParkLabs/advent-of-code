0a8eac613df5fe3c4d2f2a46d4a157379785a4ca
erik-ekberg-gd <102057982+erik-ekberg-gd@users.noreply.github.com>
2022-12-02 11:26:30 -0800

    Scaffold reading text files

--- 2022/day-1/solution-1.exs
+++ 2022/day-1/solution-1.exs
@@ -1,1 +1,8 @@
-IO.inspect("hello world")
+defmodule Day1 do
+        def read_file(file_name) do
+                {:ok, content} = File.read(file_name)
+                content
+        end
+end
+
+IO.inspect(Day1.read_file('./input.txt'))
