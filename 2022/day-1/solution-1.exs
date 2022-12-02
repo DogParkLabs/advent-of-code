f34baa206d18d737e522af5d8fdd1f9eddd9d69b
erik-ekberg-gd <102057982+erik-ekberg-gd@users.noreply.github.com>
2022-12-02 11:51:42 -0800

    Output array of summed calories

--- 2022/day-1/solution-1.exs
+++ 2022/day-1/solution-1.exs
@@ -7,6 +7,18 @@ defmodule Day1 do
         def parse_content(content) do
                 String.split(content, "\n\n")
         end
+
+        def parse_elves(content) do
+                # Enum.map(content, fn (line) -> Day1.parse_elf(line) end)
+                Enum.map(content, &Day1.parse_elf/1) # Only call parse_elf that accepts one argument
+        end
+
+        def parse_elf(line) do
+                line
+                |> String.split("\n")
+                |> Enum.map(&String.to_integer/1)
+                |> Enum.sum()
+        end
 end
 
 # IO.inspect(Day1.parse_content(Day1.read_file('./input.txt')))
@@ -14,4 +26,5 @@ end
 './input.txt'
 |> Day1.read_file()
 |> Day1.parse_content()
+|> Day1.parse_elves()
 |> IO.inspect()
