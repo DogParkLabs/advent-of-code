c2840e95cd872b273bf56498ea4a0cacbf741ba4
Donovan Peralta <donovanperalta@gmail.com>
2022-12-09 14:53:44 -0500

    Parse input file and build grid.

--- 2022/day-8/main.go
+++ 2022/day-8/main.go
@@ -3,9 +3,27 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	data, _ := os.ReadFile("./sample-input.txt")
-	fmt.Println(string(data))
+	inputStr := string(data)
+	trees := strings.Split(inputStr, "\n")
+	grid := make([][]int, len(trees))
+
+	for i, value := range trees {
+		chars := strings.Split(value, "")
+		numbers := make([]int, len(chars))
+
+		for j, char := range chars {
+			number, _ := strconv.Atoi(char)
+			numbers[j] = number
+		}
+
+		grid[i] = numbers
+	}
+
+	fmt.Println(grid)
 }
