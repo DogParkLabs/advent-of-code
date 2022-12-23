530ebfda0fd2ac2bcd42a15ae1894613e1836e70
erik-ekberg-gd <102057982+erik-ekberg-gd@users.noreply.github.com>
2022-12-23 11:27:29 -0800

    Scaffold parse input function

--- 2022/day-9/index.mjs
+++ 2022/day-9/index.mjs
@@ -1,4 +1,8 @@
 import { readFileSync } from "fs";
 
-const data = readFileSync("./example-1.txt", "utf-8");
-console.log(data);
+const parseInput = (path) => {
+  const data = readFileSync(path, "utf-8").split("\n");
+  return data;
+};
+
+console.log(parseInput("./example-1.txt"));
