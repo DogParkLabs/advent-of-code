4bd284dc4f612e5a0a1c9b9b28a4abba39da0f75
erik-ekberg-gd <102057982+erik-ekberg-gd@users.noreply.github.com>
2022-12-23 11:33:06 -0800

    Transform file content into directions and steps

--- 2022/day-9/index.mjs
+++ 2022/day-9/index.mjs
@@ -1,7 +1,10 @@
 import { readFileSync } from "fs";
 
 const parseInput = (path) => {
-  const data = readFileSync(path, "utf-8").split("\n");
+  const data = readFileSync(path, "utf-8")
+    .split("\n")
+    .map((line) => line.split(" "))
+    .map(([dir, steps]) => ({ dir, steps: parseInt(steps, 10) }));
   return data;
 };
 
